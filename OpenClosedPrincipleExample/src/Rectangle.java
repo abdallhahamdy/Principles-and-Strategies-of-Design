8c37b63dfe328311af44a84178a0296f01104a99
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2023-07-21 23:49:55 +0200

    Open-Closed-Principle

---
+++ OpenClosedPrincipleExample/src/Rectangle.java
@@ -0,0 +1,29 @@
+interface Shape {
+    public double calculateArea();
+}
+
+public class Rectangle implements Shape {
+    public double length;
+    public double width;
+
+    public double calculateArea()
+    {
+        return length * width;
+    }
+}
+
+class Circle implements Shape {
+    public double radius;
+
+    public double calculateArea(){
+        return (22/7) * radius * radius;
+    }
+}
+
+
+class AreaCalculator  {
+    public double calculateShapeArea(Shape shape) {
+        return shape.calculateArea();
+    }
+}
+
